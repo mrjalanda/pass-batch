c6c4806a8939ecec0c06788ca3787d881a785fda
mrjalanda <berichldw@gmail.com>
2023-12-21 15:35:56 +0900

    first commit

---
+++ src/test/java/org/fastdev/passbatch/PassBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fastdev.passbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
